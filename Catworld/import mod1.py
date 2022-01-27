133b9e15f1fc3cf72b55758d3773ca23091f0c6e
DuhwanLee1220 <98135982+DuhwanLee1220@users.noreply.github.com>
2022-01-27 14:39:09 +0900

    Delete Catworld directory

--- Catworld/import mod1.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append("C:\\")
-from mod1 import *
-
--- Catworld/mod1.py
+++
@@ -1,8 +0,0 @@
-#mod1.py
-def add(a,b):
-    return a+b
-
-def plusTwo(a):
-    return a + 2
-#if __name__ == "__main__":
-print(add(2,3))
