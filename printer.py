6494103972e3eee6a74d81259718ce944cc2f267
DuhwanLee1220 <98135982+DuhwanLee1220@users.noreply.github.com>
2022-01-27 10:44:51 +0900

    add printer function
    printer function ver 22.01

---
+++ printer.py
@@ -0,0 +1,9 @@
+import time
+
+def printLater():
+    time.sleep(2)
+    
+def printInfo(contents):
+    print('====================================================')
+    print(contents)
+    print('====================================================')
